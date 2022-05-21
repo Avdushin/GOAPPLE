7ff8db07899ee55bf5b9f15c9332ad8e24b17f41
Avdushin <avdushinbeaver1@gmail.com>
2022-05-21 17:44:38 +0300

    Distros scripts to lowercase| Apps rebuilded

--- code/go.mod
+++ code/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea // indirect
--- code/main.go
+++ code/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
-	"github.com/mbndr/figlet4go"
+	. "github.com/logrusorgru/aurora" // Colors
+	"github.com/mbndr/figlet4go"      // Logotype
+)
+
+var (
+	version = "2.2"
+	scaner  string
 )
 
 func clear() {
@@ -23,16 +30,55 @@ func logo() {
 	fmt.Print(renderStr)
 }
 
-func versionf() {
-	fmt.Println("GOAPPLE 2.0")
+func descr() {
+	fmt.Println(fmt.Sprint(Bold(Yellow("\t\t\tversion")), Red(version), Bold("\n\n USAGE:\n\n"), Bold("goapple-cli"), "\t\t to start CLI application\n\n", Bold(" goapple-gui"), "\t\t to start GUI application\n\n"))
 }
 
-func descr() {
-	fmt.Print(" \t\t\t\033[33mversion \033[31m2.1\033[0m\n\n USAGE:\n\n\033[1m goapple-cli\033[0m\t to start CLI application\n\n\033[1m goapple-gui\033[0m\t to start GUI application\n\n")
+func cli() {
+	cmd := exec.Command("goapple-cli")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+}
+
+func gui() {
+	cmd := exec.Command("goapple-gui")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+}
+
+func input() {
+	fmt.Print(" Type \"cli\", \"gui\" or \"quit\" => ")
+
+	fmt.Scan(&scaner)
+	if scaner == "goapple-cli" {
+		cli()
+	} else if scaner == "cli" {
+		cli()
+	} else if scaner == "goapple-gui" {
+		gui()
+	} else if scaner == "gui" {
+		gui()
+	} else if scaner == "quit" {
+		return
+	} else if scaner == "exit" {
+		return
+	} else if scaner == "0" {
+		return
+	} else {
+		clear()
+		logo()
+		descr()
+		input()
+	}
 }
 
 func main() {
 	clear()
 	logo()
 	descr()
+	input()
 }
